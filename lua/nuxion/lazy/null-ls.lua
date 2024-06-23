aafaa571038e088765d975c25fff1b8c1f8f26db
nuxion <nuxion@gmail.com>
2024-06-23 19:38:12 -0300

    gitsigns: show in file modifications
    - ruff also as formarter

--- lua/nuxion/lazy/null-ls.lua
+++ lua/nuxion/lazy/null-ls.lua
@@ -11,7 +11,7 @@ return {
         null_ls.setup({
             sources = {
                 null_ls.builtins.diagnostics.ruff,
-                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.ruff,
             }
         })
     end
