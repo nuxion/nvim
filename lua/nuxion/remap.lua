cd4e0487be2728a9029a932cefc8ca74fb9ddfd5
Xavier Petit <nuxion@gmail.com>
2026-03-30 13:49:48 -0300

    cp mapping to copy fullpath of the open file in the buffer

--- lua/nuxion/remap.lua
+++ lua/nuxion/remap.lua
@@ -1,4 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {remap=false})
+vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, { remap = false })
+-- Copy current filepath to system clipboard
+-- If you'd prefer the absolute path, change "%" to "%:p".
+vim.keymap.set("n", "cp", function()
+    local path = vim.fn.expand("%:p")
+    vim.fn.setreg("+", path)
+    vim.fn.setreg('"', path)
+    print("Copied: " .. path)
+end)
